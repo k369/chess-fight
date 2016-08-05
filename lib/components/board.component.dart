c7022099e0bd90fe5f73b601f4ffb1a0066476e4
vipous <vipous@gmail.com>
2016-08-05 12:21:35 -0700

    init

---
+++ lib/components/board.component.dart
@@ -0,0 +1,14 @@
+import 'package:angular2/angular2.dart';
+
+@Component(
+    selector: 'board',
+    templateUrl: 'board.component.html',
+    styleUrls: const ['board.component.css']
+)
+class BoardComponent extends OnInit{
+
+  @override
+  ngOnInit() {
+
+  }
+}
